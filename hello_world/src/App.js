dc2d5260b8c29d0e58df1a0751a54097a665bc1f
richardl33 <rich.leeroy@gmail.com>
2020-11-26 22:12:39 -0500

    create react Hello class

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// bring in React and Component from React
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+
+// define our Hello component
+class Hello extends Component {
+  // What should the component render?
+  render () {
+
+    // Make sure to return some UI.
+    return (
+      <h1>Hello World!</h1>
+    )
+  }
 }
 
-export default App;
+export default Hello
